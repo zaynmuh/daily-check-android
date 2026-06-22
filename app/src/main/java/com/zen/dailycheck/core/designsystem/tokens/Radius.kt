a2889266bb1260e9b28059e744252b2c6e122527
znveex <zaynmuhammad@MacBook-Zen.local>
2026-06-22 15:03:24 +0700

    DC-6 Setup Design System

---
+++ app/src/main/java/com/zen/dailycheck/core/designsystem/tokens/Radius.kt
@@ -0,0 +1,10 @@
+package com.zen.dailycheck.core.designsystem.tokens
+
+import androidx.compose.ui.unit.dp
+
+object Radius {
+    val sm = 8.dp
+    val md = 12.dp
+    val lg = 16.dp
+    val xl = 24.dp
+}
